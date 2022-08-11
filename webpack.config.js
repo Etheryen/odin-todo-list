0321389ebfefe6a908a44665429fb75909fdce61
Etheryen <lukasz.ciwoniuk@gmail.com>
2022-08-11 17:31:44 +0200

    production

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,8 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: './src/index.js',
-  devtool: 'inline-source-map',
-  devServer: {
-    static: './dist',
-  },
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
